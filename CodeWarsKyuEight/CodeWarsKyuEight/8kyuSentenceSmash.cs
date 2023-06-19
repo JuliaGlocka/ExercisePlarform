a3b2cf91a1f79ae8311e6c4bddd736fdff3141e2
Julia Glocka <glockajulia@gmail.com>
2023-06-19 10:45:22 +0200

    Another Exerices Finished From 8kyu CodeWars

---
+++ CodeWarsKyuEight/CodeWarsKyuEight/8kyuSentenceSmash.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CodeWarsKyuEight;
+public class SentenceSmash
+{
+    public static string Smash(string[] words)
+    {
+        return string.Join(" ", words);
+    }
+}
--- CodeWarsKyuEight/CodeWarsKyuEight/Program.cs
+++ CodeWarsKyuEight/CodeWarsKyuEight/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 using CodeWarsKyuEight;
+using System;
 
 Console.WriteLine("Hello, World!");
-
+Console.WriteLine(SentenceSmash.Smash(new string[] { "hello", "amazing", "world" }));
 Console.WriteLine(Clock.Past(7,35,35));
 Console.WriteLine(KataTwelve.OtherAngle(20, 30));
 
